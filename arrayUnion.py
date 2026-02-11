2d54511ff6b6bc5058f41df221e0a802c44eb110
ritika976 <ritikakr976@gmail.com>
2026-02-11 09:56:08 +0530

    Simplify array union implementation
    Refactor array union logic to improve readability.

--- arrayUnion.py
+++ arrayUnion.py
@@ -3,12 +3,10 @@ b = [9, 2, 7, 4, 6, 9]
 
 union = []
 
-# Traverse first array
 for num in a:
     if num not in union:
         union.append(num)
 
-# Traverse second array
 for num in b:
     if num not in union:
         union.append(num)
