f3dc9cb4461bfdc7af88cd6fce3899512c22772f
ritika976 <ritikakr976@gmail.com>
2026-02-20 01:10:55 +0530

    Implement factorial function in Solution class

---
+++ factorial.py
@@ -0,0 +1,8 @@
+class Solution:
+    def factorial(self, n):
+        fact = 1
+        
+        for i in range(2, n + 1):
+            fact *= i
+        
+        return list(map(int, str(fact)))
